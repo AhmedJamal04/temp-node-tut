0a6b56aa863533801a8e99fae09aea0fc3157287
AhmedJamal04 <82374677+AhmedJamal04@users.noreply.github.com>
2021-04-12 15:42:04 +0500

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+//Common JS --> Every file is a module (by default)
+//MODULES --> Encapsulated code (only share with)
+
+const names = require ('./4-names')
+const sayHi = require ('./5-utils')
+const data = require ('./6-alternative-flavor')
+require(`./7-mind-grenade`)
+sayHi(`susan`)
+sayHi(names.ahmed)
+sayHi(names.jamal)
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'SUPER SECRET'
+//share 
+const ahmed = 'ahmed'
+const jamal = 'jamal'
+
+module.exports = {ahmed,jamal}
